04e6cdd38645a5bd643ad0a3375e7f684947f5f6
JosePerez013 <joseperez951@icloud.com>
2023-08-09 19:39:07 -0700

    adding css along with quiz functionality

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,60 +1,25 @@
-[
+var questions = [
   {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses",
+    title: "What punctuation mark is used to end a statement in JavaScript?",
+    choices: ["Period", "Comma", "Semicolon", "Colon"],
+    answer: "Semicolon",
   },
   {
     title:
-      "In JavaScript, which function is used to convert a string to an integer?",
-    choices: ["parseInt", "parseFloat", "stringToInt", "toInt"],
-    answer: "parseInt",
+      "Which keyword is used to declare a constant variable in JavaScript?",
+    choices: ["let", "var", "const", "variable"],
+    answer: "const",
   },
   {
     title:
-      "Which array method is used to add one or more elements to the end of an array and returns the new length of the array?",
-    choices: ["push", "append", "addToEnd", "concat"],
-    answer: "push",
-  },
-  {
-    title: 'What is the purpose of the "typeof" operator in JavaScript?',
-    choices: [
-      "To check if a variable is defined",
-      "To determine the type of a value",
-      "To compare two values",
-      "To perform arithmetic operations",
-    ],
-    answer: "To determine the type of a value",
+      "What built-in function is used to print output to the console in JavaScript?",
+    choices: ["console.log", "print", "display", "output"],
+    answer: "console.log",
   },
   {
     title:
-      'In JavaScript, what does the "this" keyword refer to in the context of an object\'s method?',
-    choices: [
-      "The global window object",
-      "The parent object",
-      "The current function",
-      "The method itself",
-    ],
-    answer: "The parent object",
-  },
-  {
-    title: "What keyword is used to declare a variable in JavaScript?",
-    choices: ["variable", "var", "declare", "let"],
-    answer: "var",
-  },
-  {
-    title: "Which data type represents a true or false value in JavaScript?",
-    choices: ["boolean", "string", "number", "undefined"],
-    answer: "boolean",
-  },
-  {
-    title: "What is the purpose of the 'console.log()' function?",
-    choices: [
-      "Prints messages to the console for debugging",
-      "Adds elements to an array",
-      "Performs mathematical calculations",
-      "Creates new HTML elements",
-    ],
-    answer: "Prints messages to the console for debugging",
+      "Which operator is used to concatenate (combine) strings in JavaScript?",
+    choices: ["+", "-", "*", "&", "/"],
+    answer: "+",
   },
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,5 +13,134 @@ var time = questions.length * 15;
 var timerId;
 
 // sfx sounds
-var sfxRight = new Audio("assets/sfx/sectionfail.wav");
-var sfxWrong = new Audio("assets/sfx/sectionpass.wav");
+var sfxRight = new Audio("assets/sfx/sectionpass.wav");
+var sfxWrong = new Audio("assets/sfx/sectionfail.wav");
+
+// start quiz function
+function quizStart() {
+  var startScreenEl = document.getElementById("start-screen");
+  // hide screen
+  startScreenEl.setAttribute("class", "hide");
+  // unhide questions
+  questionsEl.removeAttribute("class");
+  // start timer
+  timerId = setInterval(clockTick, 1000);
+  // show starting time
+  timerEl.textContent = time;
+
+  getQuestion();
+}
+
+// function to get qustions
+function getQuestion() {
+  const currentQuestion = questions[currentQuestionIndex];
+// this will change the title of the question
+  const titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+
+  choicesEl.innerHTML = "";
+
+  currentQuestion.choices.forEach((choice, index) => {
+    const choiceNode = document.createElement("button");
+    choiceNode.classList.add("choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = `${index + 1}. ${choice}`;
+
+    choicesEl.appendChild(choiceNode);
+  });
+}
+
+function questionClick(event) {
+  var buttonEl = event.target;
+  // if the clicked element is not a choice button, do nothing.
+  if (!buttonEl.matches(".choice")) {
+    return;
+  }
+  // check if user guessed wrong
+  if (buttonEl.value !== questions[currentQuestionIndex].answer) {
+    // penalize time
+    time -= 15;
+    if (time < 0) {
+      time = 0;
+    }
+    // display new time on page
+    timerEl.textContent = time;
+    // play "wrong" sound effect
+    sfxWrong.play();
+    feedbackEl.textContent = "Wrong!";
+  } else {
+    // play "right" sound effect
+    sfxRight.play();
+    feedbackEl.textContent = "Correct!";
+  }
+
+  // flash right/wrong feedback on page for half a second
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function () {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+  // move to next question
+  currentQuestionIndex++;
+  // check if we've run out of questions
+  if (time <= 0 || currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+function quizEnd() {
+  // stop timer
+  clearInterval(timerId);
+  // show end screen
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
+  // show final score
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
+  // hide questions section
+  questionsEl.setAttribute("class", "hide");
+}
+
+function clockTick() {
+  // update time
+  time--;
+  timerEl.textContent = time;
+  // check if user ran out of time
+  if (time <= 0) {
+    quizEnd();
+  }
+}
+
+function saveHighscore() {
+  const initials = initialsEl.value.trim();
+// this will check if initials are entered
+  if (initials !== "") {
+    const highscores = JSON.parse(localStorage.getItem("scores")) || [];
+// create new score object
+    const newScore = {
+      score: time,
+      initials: initials,
+    };
+
+    highscores.push(newScore);
+// add to localstorage
+    localStorage.setItem("scores", JSON.stringify(highscores));
+
+    window.location.href = "scores.html";
+  }
+}
+
+// this function checks for enter key when user is entering initials
+function checkForEnter(event) {
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+// clicks for buttons
+submitBtn.onclick = saveHighscore;
+startBtn.onclick = quizStart;
+choicesEl.onclick = questionClick;
+initialsEl.onkeyup = checkForEnter;
