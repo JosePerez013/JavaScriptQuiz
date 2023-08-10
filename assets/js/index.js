efc0f7305d99a2f63ea68ddb0bf6187fde9e4d7e
JosePerez013 <joseperez951@icloud.com>
2023-08-09 17:48:49 -0700

    added HTML for the main page and Scores Page along with Questions for the quiz

---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+[
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    title:
+      "In JavaScript, which function is used to convert a string to an integer?",
+    choices: ["parseInt", "parseFloat", "stringToInt", "toInt"],
+    answer: "parseInt",
+  },
+  {
+    title:
+      "Which array method is used to add one or more elements to the end of an array and returns the new length of the array?",
+    choices: ["push", "append", "addToEnd", "concat"],
+    answer: "push",
+  },
+  {
+    title: 'What is the purpose of the "typeof" operator in JavaScript?',
+    choices: [
+      "To check if a variable is defined",
+      "To determine the type of a value",
+      "To compare two values",
+      "To perform arithmetic operations",
+    ],
+    answer: "To determine the type of a value",
+  },
+  {
+    title:
+      'In JavaScript, what does the "this" keyword refer to in the context of an object\'s method?',
+    choices: [
+      "The global window object",
+      "The parent object",
+      "The current function",
+      "The method itself",
+    ],
+    answer: "The parent object",
+  },
+  {
+    title: "What keyword is used to declare a variable in JavaScript?",
+    choices: ["variable", "var", "declare", "let"],
+    answer: "var",
+  },
+  {
+    title: "Which data type represents a true or false value in JavaScript?",
+    choices: ["boolean", "string", "number", "undefined"],
+    answer: "boolean",
+  },
+  {
+    title: "What is the purpose of the 'console.log()' function?",
+    choices: [
+      "Prints messages to the console for debugging",
+      "Adds elements to an array",
+      "Performs mathematical calculations",
+      "Creates new HTML elements",
+    ],
+    answer: "Prints messages to the console for debugging",
+  },
+];
